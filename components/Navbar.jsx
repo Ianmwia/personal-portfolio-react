559997c7b57a91be0e5edcd231f307eba567b476
Mwia <iansnr10@gmail.com>
2025-10-23 16:19:58 +0300

    styling the nav links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,15 @@
 function Navbar(){
     return(
         <>
-        <nav className='bg-gray-900 text-white flex justify-between px-2.5' >
+        <nav className='bg-gray-900 text-white flex justify-between p-2.5' >
             <div>
                 <p className='text-2xl font-bold'>My Portfolio</p>
             </div>
-            <ul className='hidden'>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">Contact</a></li>
-                
+            <ul className='flex space-x-3'>
+                <li className='hover:scale-105 transition duration-300 hover:decoration-amber-400 hover:underline'><a href="">About</a></li>
+                <li className='hover:scale-105 transition duration-300 hover:decoration-amber-400 hover:underline'><a href="">Projects</a></li>
+                <li className='hover:scale-105 transition duration-300 hover:decoration-amber-400 hover:underline'><a href="">Blog</a></li>
+                <li className='hover:scale-105 transition duration-300 hover:decoration-amber-400 hover:underline'><a href="">Contact</a></li>
             </ul>
         </nav>
         </>
