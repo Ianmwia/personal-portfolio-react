fab57d6f82f6f78b6f5a7a8fc9a74cde3b97b14d
Mwia <iansnr10@gmail.com>
2025-10-23 22:03:20 +0300

    creating the home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,9 @@ function Home(){
     return(
         <>
         <main>
-            <h1>Home</h1>
+            <div className="m-3 p-4">
+            <p className='text-left font-bold tracking-tight text-7xl text-balance'>Lets Create something <span className='bg-linear-to-r from-pink-500 via-yellow-400 to-blue-500 bg-300% bg-clip-text text-transparent animate-gradient-slide'>Memorable</span> together</p>
+            </div>
         </main>
         </>
     )
--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,8 +1,19 @@
+import weather from '../src/assets/weather.jpg'
+
 function Projects(){
     return(
         <>
-        <main>
+        <main className='m-2.5'>
             <h1>Projects</h1>
+            <div className='border rounded w-48 h-72 flex flex-col'>
+                <a href="https://weather-now-capstone.netlify.app/">
+                <img src={weather} alt="weather" className='w-full h-2/3 object-cover'/>
+                <div className='h-1/3 p-2 overflow-auto'>
+                    <p className='font-medium text-lg'>Weather App</p>
+                    <p className='text-sm'>Weather app to get the current weather, hourly forecast and weather forecast</p>
+                    </div>
+                </a>
+            </div>
         </main>
         </>
     )
